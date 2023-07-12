aa95e82163b800c4a80223c4db752afbb96d6238
lbenjaminq <benjaminlbq12@gmail.com>
2023-07-11 22:09:28 -0300

    Refactor: change in database

--- database/src/controllers/index.js
+++ database/src/controllers/index.js
@@ -1,15 +1,14 @@
 const store = require("../database");
-const { catchedAsync } = require("../utils");
 
 module.exports = {
-  listCharacters: catchedAsync(async (req, res) => {
+  listCharacters: async (req, res) => {
     const { model } = req.params;
     const response = await store[model].list();
     res.status(200).json(response);
-  }),
-  getChacter: catchedAsync(async (req, res) => {
+  },
+  getChacter: async (req, res) => {
     const { model, id } = req.params;
     const response = await store[model].get(id);
     res.status(200).json(response);
-  }),
+  },
 };
--- database/src/server.js
+++ database/src/server.js
@@ -8,8 +8,12 @@ server.use(morgan("dev"));
 
 server.use(require("./routes"));
 
+server.use("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
 server.use((err, req, res, next) => {
-  res.status(err.statusCode).send(err.message);
+  res.status(err.statusCode || 500).send(err.message);
 });
 
 module.exports = server;

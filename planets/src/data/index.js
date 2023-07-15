9e48da469daeafc0418dbada27364ae0a5e4535f
lbenjaminq <benjaminlbq12@gmail.com>
2023-07-15 18:13:36 -0300

    Fix: fix ./src/data

--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -3,22 +3,19 @@ const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    // const planets = await axios.get("http://database:8004/Character");
-    // return planets.data;
-    return planets;
+    const planets = await axios.get("http://database:8004/Character");
+    return planets.data;
   },
   create: async (planet) => {
-    //   try {
-    //     const planets = await axios.post(
-    //       "http://database:8004/Character",
-    //       planet
-    //     );
-    //     return planets.data;
-    //   } catch (error) {
-    //     console.error(error);
-    //     throw new Error("Error creating film");
-    //   }
-    // },
-    return planet;
+    try {
+      const planets = await axios.post(
+        "http://database:8004/Character",
+        planet
+      );
+      return planets.data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error creating film");
+    }
   },
 };

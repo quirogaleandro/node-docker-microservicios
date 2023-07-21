72dead0a87e996f1f0c61ede4a7a7fa325771f4d
lbenjaminq <benjaminlbq12@gmail.com>
2023-07-21 12:34:11 -0300

    Fix: path in data/index.js

--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -2,19 +2,16 @@ const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    const planets = await axios.get("http://database:8004/Character");
+    const planets = await axios.get("http://database:8004/Planet");
     return planets.data;
   },
   get: async (id) => {
-    const planets = await axios.get("http://database:8004/Character/" + id);
+    const planets = await axios.get("http://database:8004/Planet/" + id);
     return planets.data;
   },
   create: async (planet) => {
     try {
-      const planets = await axios.post(
-        "http://database:8004/Character",
-        planet
-      );
+      const planets = await axios.post("http://database:8004/Planet", planet);
       return planets.data;
     } catch (error) {
       console.error(error);

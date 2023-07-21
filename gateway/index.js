9a9858be324a09514022920804d5d59be492f9f5
lbenjaminq <benjaminlbq12@gmail.com>
2023-07-21 11:39:53 -0300

    Fix: changes in gateway index.js

--- gateway/index.js
+++ gateway/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(morgan("dev"));
+
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN,
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+};
+
+app.use(cors(corsOptions));
 
 app.use(
   "/characters",
